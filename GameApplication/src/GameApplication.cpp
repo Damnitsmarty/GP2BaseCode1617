96bd84934cfa161d80783ec5f92143ef0eb5df1b
Smarty <MKOLEV200@caledonian.ac.uk>
2016-10-06 12:08:51 +0100

    Creating a GL window

--- GameApplication/src/GameApplication.cpp
+++ GameApplication/src/GameApplication.cpp
@@ -5,11 +5,11 @@ GameApplication::GameApplication()
  	m_pWindow=nullptr;
 	m_WindowWidth=640;
 	m_WindowHeight=480;
-	m_WindowCreationFlags=0;
+	m_WindowCreationFlags=SDL_WINDOW_OPENGL;
 	CREATELOG("log.txt");
 	m_bIsActive=false;
 	m_bIsRunning=false;
-  m_WindowTitle="Game";
+	m_WindowTitle="Game";
 }
 
 GameApplication::~GameApplication()
@@ -32,6 +32,62 @@ void GameApplication::createWindow(const string& windowTitle,const unsigned int 
 
 		m_WindowWidth=width;
 		m_WindowHeight=height;
+}
+
+void GameApplication::OnBeginRender()
+{
+}
+
+void GameApplication::render()
+{
+}
+
+void GameApplication::OnEndRender()
+{
+}
+
+void GameApplication::update()
+{
+}
+
+void GameApplication::initGraphics()
+{
+	//OpenGl Context
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+		SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	m_GLcontext = SDL_GL_CreateContext(m_pWindow);
+
+	//GLEW
+	glewExperimental = GL_TRUE;
+
+	GLenum err = glewInit();
+
+	if (GLEW_OK != err) {
+		LOG(ERROR, "Error: %s", glewGetErrorString(err));
+	}
+
+	//OpenGL States
+	//Smooth shading
+	glShadeModel(GL_SMOOTH);
+
+	//clear the background to black
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	//Clear the depth buffer to 1.0
+	glClearDepth(1.0f);
+
+	//Enable depth testing
+	glEnable(GL_DEPTH_TEST);
+
+	//The depth test to use
+	glDepthFunc(GL_LEQUAL);
+
+	//Turn on best perspective correction
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
 }
 
 void GameApplication::parseConfig(int args,char * arg[])
@@ -58,7 +114,7 @@ void GameApplication::parseConfig(int args,char * arg[])
 
 bool GameApplication::init(int args,char * arg[])
 {
-  parseConfig(args,arg);
+	parseConfig(args,arg);
 
 	//Controls the game loop
 	m_bIsRunning = true;
@@ -74,9 +130,9 @@ bool GameApplication::init(int args,char * arg[])
 
 	m_WindowWidth=m_Options.getOptionAsInt("WindowWidth");
 	m_WindowHeight=m_Options.getOptionAsInt("WindowHeight");
-  m_WindowTitle=m_Options.getOption("WindowTitle");
+	m_WindowTitle=m_Options.getOption("WindowTitle");
 	createWindow(m_WindowTitle,m_WindowWidth,m_WindowHeight,m_WindowCreationFlags);
-
+	initGraphics();
 
 	m_bIsActive=true;
 	return true;
@@ -86,6 +142,8 @@ void GameApplication::OnQuit()
 {
 	//set our boolean which controls the loop to false
 	m_bIsRunning = false;
+
+	SDL_GL_DeleteContext(m_GLcontext);
 	SDL_DestroyWindow(m_pWindow);
 	SDL_Quit();
 	CLOSELOG();
@@ -164,5 +222,13 @@ void GameApplication::run()
 				}
 			}
 		}
+
+		update();
+
+		OnBeginRender();
+		render();
+		OnEndRender();
+
 	}
+
 }
