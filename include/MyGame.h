89b85603540ee93e999a308d23412b05b29c56f2
Smarty <MKOLEV200@caledonian.ac.uk>
2016-10-13 12:53:29 +0100

    Added Shaders

--- include/MyGame.h
+++ include/MyGame.h
@@ -6,12 +6,21 @@
 class MyGame:public GameApplication
 {
 public:
+	GLuint m_ShaderProgram;
+	
 	MyGame();
+	
+
 	~MyGame();
 	void initScene();
 	void destroyScene();
 	void render();
 	void update();
+
+	//matrices
+	mat4 m_ViewMatrix;
+	mat4 m_ProjMatrix;
+	mat4 m_ModelMatrix;
 protected:
 	GLuint m_VBO;
 	GLuint m_VAO;
--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -1,4 +1,6 @@
 #include "MyGame.h"
+const std::string ASSET_PATH = "assets";
+const std::string SHADER_PATH = "/shaders";
 
 struct Vertex {
 	float x, y, z;
@@ -36,16 +38,50 @@ void MyGame::initScene()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
 
+	//Define shader objects
+	GLuint vertexShaderProgram = 0;
+	std::string vsPath = ASSET_PATH + SHADER_PATH + "/simpleVS.glsl";
+	vertexShaderProgram = loadShaderFromFile(vsPath, VERTEX_SHADER);
+
+	GLuint fragmentShaderProgram = 0;
+	std::string fsPath = ASSET_PATH + SHADER_PATH + "/simpleFS.glsl";
+	fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
+
+	//Create program
+	m_ShaderProgram = glCreateProgram();
+	glAttachShader(m_ShaderProgram, vertexShaderProgram);
+	glAttachShader(m_ShaderProgram, fragmentShaderProgram);
+	glLinkProgram(m_ShaderProgram);
+	checkForLinkErrors(m_ShaderProgram);
+	logShaderInfo(m_ShaderProgram);
+
+	//Delete shader objects
+	glDeleteShader(vertexShaderProgram);
+	glDeleteShader(fragmentShaderProgram);
 }
 
 void MyGame::destroyScene()
 {
+	glDeleteProgram(m_ShaderProgram);
 }
 
 void MyGame::render()
 {
+	glUseProgram(m_ShaderProgram);
+	glBindVertexArray(m_VAO);
+
+	GLint MVPLocation = glGetUniformLocation(m_ShaderProgram, "MVP");
+	if (MVPLocation != -1)
+	{
+		mat4 MVP = m_ProjMatrix*m_ViewMatrix*m_ModelMatrix;
+		glUniformMatrix4fv(MVPLocation, 1,GL_FALSE, glm::value_ptr(MVP));
+	}
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 void MyGame::update()
 {
+	m_ProjMatrix = perspective(radians(45.0f), (float)m_WindowWidth / (float)m_WindowHeight, 0.1f, 100.0f);
+	m_ViewMatrix = lookAt(vec3(0.0f, 0.0f, 10.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+	m_ModelMatrix = translate(mat4(1.0f), vec3(0.0f, 0.0f, -1.0f));
 }
